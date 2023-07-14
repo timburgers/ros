2196506a61943505c14d4458a6ddcdf5990dcf6d
Tim Burgers <tim-burgers@hotmail.com>
2023-07-14 15:24:55 +0200

    added fixes

--- snnblimp_ws/src/motor_control/src/href.py
+++ snnblimp_ws/src/motor_control/src/href.py
@@ -2,19 +2,20 @@
 import rospy
 from std_msgs.msg import Float32
 
-FREQUENCY = 1
+FREQUENCY = 1.0
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    rospy.loginfo("Generate h ref node has been started")
     rospy.init_node("generate_h_ref")
     rospy.loginfo("Generate h ref node has been started")
 
-    pub_h_ref = rospy.Publisher("/h_ref",Float32, queue_size=10)
+    pub_h_ref = rospy.Publisher("/h_ref",Float32, queue_size=5)
 
     rate = rospy.Rate(FREQUENCY)
 
     while not rospy.is_shutdown():
         msg=Float32()
-        msg= 1
+        msg.data= 1.0
         pub_h_ref.publish(msg)
         rate.sleep()
 
