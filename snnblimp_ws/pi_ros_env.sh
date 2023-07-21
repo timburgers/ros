7cdd440237022fc610eed6a7e7a806515c03cfeb
Tim Burgers <tim-burgers@hotmail.com>
2023-07-21 11:30:20 +0200

    f

--- snnblimp_ws/pi_ros_env.sh
+++ snnblimp_ws/pi_ros_env.sh
@@ -4,6 +4,7 @@ export ROS_IP=mypi
 export ROS_MASTER_URI=http://mycomp:11311
 #export ROSLAUNCH_SSH_UNKNOWN=1
 
+source /opt/ros/kinetic/setup.bash
 source /home/pi/ros/snnblimp_ws/devel/setup.bash
 
 exec "$@"
