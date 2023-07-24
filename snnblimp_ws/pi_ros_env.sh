513cad699085b85d6273235234d7688abca10bcf
Tim Burgers <tim-burgers@hotmail.com>
2023-07-24 17:56:57 +0200

    added new files

--- snnblimp_ws/pi_ros_env.sh
+++ snnblimp_ws/pi_ros_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export ROS_IP=mypi
-export ROS_MASTER_URI=http://mycomp:11311
+export ROS_IP=192.168.12.35
+export ROS_MASTER_URI=http://192.168.12.1:11311
 #export ROSLAUNCH_SSH_UNKNOWN=1
 
-source /opt/ros/kinetic/setup.bash
+source /opt/ros/noetic/setup.bash
 source /home/pi/ros/snnblimp_ws/devel/setup.bash
 
 exec "$@"
