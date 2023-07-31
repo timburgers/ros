5173ddc4ed2cebf7d7fbd4175b958f07962e16f7
Tim Burgers <tim-burgers@hotmail.com>
2023-07-31 10:27:19 +0200

    new changes

--- snnblimp_ws/src/motor_control/src/controller_full.py
+++ snnblimp_ws/src/motor_control/src/controller_full.py
@@ -55,7 +55,7 @@ class Controller:
         self.error = 0.0
 
         # Controllers
-        self.pid = PID.PID(6, 0.4, 0.9, 1/FREQUENCY, True) # self.pid = PID.PID(P, I, D, dt, simple)
+        self.pid = PID.PID(4, 0.6, 7, 1/FREQUENCY, True) # self.pid = PID.PID(P, I, D, dt, simple)
         
         # SNN
         # self.init_SNN_model()
