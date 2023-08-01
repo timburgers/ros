809cb31897a9dfde747126046448c9df5f436e0b
Tim Burgers <tim-burgers@hotmail.com>
2023-08-01 12:12:41 +0200

    new

--- snnblimp_ws/pi_ros_env.sh
+++ snnblimp_ws/pi_ros_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ROS_IP=192.168.12.35
-export ROS_MASTER_URI=http://192.168.12.1:11311
+export ROS_MASTER_URI=http://192.168.12.35:11311
 #export ROSLAUNCH_SSH_UNKNOWN=1
 
 source /opt/ros/noetic/setup.bash
