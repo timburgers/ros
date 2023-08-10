1ff4256d83224c1083c98498dd5cb4297d3bdfcf
Tim Burgers <tim-burgers@hotmail.com>
2023-08-10 12:17:23 +0200

    new dataset

--- snnblimp_ws/src/motor_control/src/controller_full.py
+++ snnblimp_ws/src/motor_control/src/controller_full.py
@@ -29,7 +29,7 @@ from motor_control.msg import SNN_seperate
 
 # Global variables:
 FREQUENCY = 5.0
-MODE = "pid_3d"        #either "pid" or "pid_3d" or "pid_4d" or "snn" or "snn_sep" or "snn_pid"
+MODE = "pid_4d"        #either "pid" or "pid_3d" or "pid_4d" or "snn" or "snn_sep" or "snn_pid"
 
 # Only applicable if MODE == "pid"
 P = 10
