96ba6c26272a821da9029a45a84c2d0b8710cdf6
Tim Burgers <tim-burgers@hotmail.com>
2023-08-11 11:26:37 +0200

    n

--- snnblimp_ws/src/motor_control/src/controller_full.py
+++ snnblimp_ws/src/motor_control/src/controller_full.py
@@ -32,9 +32,9 @@ FREQUENCY = 10.0
 MODE = "pid"        #either "pid" or "pid_3m" or "pid_4m" or "pid_h" or "snn" or "snn_sep" or "snn_pid"
 
 # Only applicable if MODE == "pid"
-P = 5
-I = 0.5
-D = 5
+P = 6
+I = 0.15
+D = 11
 
 #Only applicable if MODE == "snn"
 SNN_FULL = "271-prime-bee"
