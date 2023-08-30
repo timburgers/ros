995f56a42e79044518dc412d26c16487e1b4f550
Tim Burgers <tim-burgers@hotmail.com>
2023-08-30 10:04:02 +0200

    n

--- snnblimp_ws/src/motor_control/src/controller_full.py
+++ snnblimp_ws/src/motor_control/src/controller_full.py
@@ -29,15 +29,15 @@ from motor_control.msg import SNN_seperate
 
 # Global variables:
 FREQUENCY = 10.0
-MODE = "pid"        #either "pid" or "pid_xm" (x=[3,4]) or "pid_h" or "snn"
+MODE = "snn"        #either "pid" or "pid_xm" (x=[3,4]) or "pid_h" or "snn"
 
-P = 9
+P = 0
 I = 0
-D = 14
+D = 0
 
 #Only applicable if MODE == "snn" NOTE: when None is specified, the other controllers will use PID
 SNN_PID = None          # Will override the P, I and D variables
-SNN_PD = None # "655-brisk-sun" # Will override the P and D varaibles
+SNN_PD = "655-brisk-sun" # Will override the P and D varaibles
 SNN_P = None
 SNN_I = None
 SNN_D = None
