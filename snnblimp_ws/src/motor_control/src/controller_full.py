efc521379fb9c9d2e25ad0abf2c333e6ef58ebfb
Tim Burgers <tim-burgers@hotmail.com>
2023-09-01 19:58:37 +0200

    n

--- snnblimp_ws/src/motor_control/src/controller_full.py
+++ snnblimp_ws/src/motor_control/src/controller_full.py
@@ -37,7 +37,7 @@ D = 0
 
 #Only applicable if MODE == "snn" NOTE: when None is specified, the other controllers will use PID
 SNN_PID = None          # Will override the P, I and D variables
-SNN_PD = "1039-celestial-wind" #"1015-leafy-yogurt" #"655-brisk-sun" # Will override the P and D varaibles
+SNN_PD = "900-wobbly-totem" #"1015-leafy-yogurt" #"655-brisk-sun" # Will override the P and D varaibles
 SNN_P = None
 SNN_I = None
 SNN_D = None
